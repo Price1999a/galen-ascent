6d6a8f48c40c1f28d01e8939a3d5aebfb55b2d12
Shen Tianqi <1055862487@qq.com>
2024-03-11 20:12:25 +0800

    正确的增量等价输入

--- src/main.rs
+++ src/main.rs
@@ -161,12 +161,12 @@ fn main() {
     println!("s{}", vec_s.len());
     println!();
 
-    let (vec_p_first, vec_p_last) = vec_p.split_at(vec_p.len() - 10);
-    let (vec_q_first, vec_q_last) = vec_q.split_at(vec_q.len() - 10);
-    let (vec_r_first, vec_r_last) = vec_r.split_at(vec_r.len() - 10);
-    let (vec_c_first, vec_c_last) = vec_c.split_at(vec_c.len() - 10);
-    let (vec_u_first, vec_u_last) = vec_u.split_at(vec_u.len() - 10);
-    let (vec_s_first, vec_s_last) = vec_s.split_at(vec_s.len() - 10);
+    let (vec_p_first, _) = vec_p.clone().split_at(vec_p.len() - 10);
+    let (vec_q_first, _) = vec_q.clone().split_at(vec_q.len() - 10);
+    let (vec_r_first, _) = vec_r.clone().split_at(vec_r.len() - 10);
+    let (vec_c_first, _) = vec_c.clone().split_at(vec_c.len() - 10);
+    let (vec_u_first, _) = vec_u.clone().split_at(vec_u.len() - 10);
+    let (vec_s_first, _) = vec_s.clone().split_at(vec_s.len() - 10);
 
     let vec_p_first_owned = vec_p_first.to_vec();
     let vec_q_first_owned = vec_q_first.to_vec();
@@ -175,12 +175,12 @@ fn main() {
     let vec_u_first_owned = vec_u_first.to_vec();
     let vec_s_first_owned = vec_s_first.to_vec();
 
-    let vec_p_last_owned = vec_p_last.to_vec();
-    let vec_q_last_owned = vec_q_last.to_vec();
-    let vec_r_last_owned = vec_r_last.to_vec();
-    let vec_c_last_owned = vec_c_last.to_vec();
-    let vec_u_last_owned = vec_u_last.to_vec();
-    let vec_s_last_owned = vec_s_last.to_vec();
+    // let vec_p_last_owned = vec_p_last.to_vec();
+    // let vec_q_last_owned = vec_q_last.to_vec();
+    // let vec_r_last_owned = vec_r_last.to_vec();
+    // let vec_c_last_owned = vec_c_last.to_vec();
+    // let vec_u_last_owned = vec_u_last.to_vec();
+    // let vec_s_last_owned = vec_s_last.to_vec();
 
     println!("initial: ");
     let start = Instant::now();
@@ -197,78 +197,72 @@ fn main() {
 
     println!("inc 1");
     let start = Instant::now();
-    program_inc1.p.extend(vec_p_first_owned.clone());
+    program_inc1.p.extend(vec_p.clone());
     program_inc1.q.extend(vec_q_first_owned.clone());
     program_inc1.r.extend(vec_r_first_owned.clone());
     program_inc1.c.extend(vec_c_first_owned.clone());
     program_inc1.u.extend(vec_u_first_owned.clone());
     program_inc1.s.extend(vec_s_first_owned.clone());
-    program_inc1.p.extend(vec_p_last_owned);
     program_inc1.run();
     let duration = start.elapsed();
     println!("方法运行时间: {:?}", duration);
 
     println!("inc 2");
     let start = Instant::now();
-    program_inc2.p.extend(vec_p_first_owned.clone());
-    program_inc2.q.extend(vec_q_first_owned.clone());
+    program_inc2.p.extend(vec_p.clone());
+    program_inc2.q.extend(vec_q.clone());
     program_inc2.r.extend(vec_r_first_owned.clone());
     program_inc2.c.extend(vec_c_first_owned.clone());
     program_inc2.u.extend(vec_u_first_owned.clone());
     program_inc2.s.extend(vec_s_first_owned.clone());
-    program_inc2.q.extend(vec_q_last_owned);
     program_inc2.run();
     let duration = start.elapsed();
     println!("方法运行时间: {:?}", duration);
 
     println!("inc 3");
     let start = Instant::now();
-    program_inc3.p.extend(vec_p_first_owned.clone());
-    program_inc3.q.extend(vec_q_first_owned.clone());
-    program_inc3.r.extend(vec_r_first_owned.clone());
+    program_inc3.p.extend(vec_p.clone());
+    program_inc3.q.extend(vec_q.clone());
+    program_inc3.r.extend(vec_r.clone());
     program_inc3.c.extend(vec_c_first_owned.clone());
     program_inc3.u.extend(vec_u_first_owned.clone());
     program_inc3.s.extend(vec_s_first_owned.clone());
-    program_inc3.r.extend(vec_r_last_owned);
     program_inc3.run();
     let duration = start.elapsed();
     println!("方法运行时间: {:?}", duration);
 
     println!("inc 4");
     let start = Instant::now();
-    program_inc4.p.extend(vec_p_first_owned.clone());
-    program_inc4.q.extend(vec_q_first_owned.clone());
-    program_inc4.r.extend(vec_r_first_owned.clone());
-    program_inc4.c.extend(vec_c_first_owned.clone());
+    program_inc4.p.extend(vec_p.clone());
+    program_inc4.q.extend(vec_q.clone());
+    program_inc4.r.extend(vec_r.clone());
+    program_inc4.c.extend(vec_c.clone());
     program_inc4.u.extend(vec_u_first_owned.clone());
     program_inc4.s.extend(vec_s_first_owned.clone());
-    program_inc4.c.extend(vec_c_last_owned);
     program_inc4.run();
     let duration = start.elapsed();
     println!("方法运行时间: {:?}", duration);
 
     println!("inc 5");
     let start = Instant::now();
-    program_inc5.p.extend(vec_p_first_owned.clone());
-    program_inc5.q.extend(vec_q_first_owned.clone());
-    program_inc5.r.extend(vec_r_first_owned.clone());
-    program_inc5.c.extend(vec_c_first_owned.clone());
-    program_inc5.u.extend(vec_u_first_owned.clone());
+    program_inc5.p.extend(vec_p.clone());
+    program_inc5.q.extend(vec_q.clone());
+    program_inc5.r.extend(vec_r.clone());
+    program_inc5.c.extend(vec_c.clone());
+    program_inc5.u.extend(vec_u.clone());
     program_inc5.s.extend(vec_s_first_owned.clone());
-    program_inc5.u.extend(vec_u_last_owned);
     program_inc5.run();
     let duration = start.elapsed();
     println!("方法运行时间: {:?}", duration);
 
     println!("inc 6");
     let start = Instant::now();
-    program_inc6.p.extend(vec_p_first_owned.clone());
-    program_inc6.q.extend(vec_q_first_owned.clone());
-    program_inc6.r.extend(vec_r_first_owned.clone());
-    program_inc6.c.extend(vec_c_first_owned.clone());
-    program_inc6.u.extend(vec_u_first_owned.clone());
-    program_inc6.s.extend(vec_s_first_owned.clone());
-    program_inc6.s.extend(vec_s_last_owned);
+    program_inc6.p.extend(vec_p.clone());
+    program_inc6.q.extend(vec_q.clone());
+    program_inc6.r.extend(vec_r.clone());
+    program_inc6.c.extend(vec_c.clone());
+    program_inc6.u.extend(vec_u.clone());
+    program_inc6.s.extend(vec_s.clone());
     program_inc6.run();
     let duration = start.elapsed();
     println!("方法运行时间: {:?}", duration);
