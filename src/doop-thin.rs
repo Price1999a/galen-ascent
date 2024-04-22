4b028a5593c53f4a7b32bb4edccc9edb559e8e25
Shen Tianqi <1055862487@qq.com>
2024-04-22 19:34:14 +0800

    正在翻译规则组中 目前到达：`.decl _InterfaceType(?interface:symbol)` 预期可能还得两天才能上服务器试着跑 在不遇见奇怪bug的情况下

--- src/doop-thin.rs
+++ src/doop-thin.rs
@@ -26,23 +26,147 @@ ascent_par! {
     relation Method_Descriptor(String, String);
     relation Method_Modifier(String, String);
 
+    relation FormalParam(String, String, String);
+    relation ThisVar(String, String);
+    relation Var_Type(String, String);
+    relation Var_DeclaringMethod(String, String);
+    relation HeapAllocation_Type(String, String);
+    relation MainMethodArgArray(String);
+    relation MainMethodArgArrayContent(String);
+    relation Instruction_Method(String, String);
+
+    relation isVirtualMethodInvocation_Insn(String);
+    relation isStaticMethodInvocation_Insn(String);
+    relation FieldInstruction_Signature(String, String);
+    relation LoadInstanceField_Base(String, String);
+    relation LoadInstanceField_To(String, String);
+    relation StoreInstanceField_From(String, String);
+    relation StoreInstanceField_Base(String, String);
+    relation LoadStaticField_To(String, String);
+    relation StoreStaticField_From(String, String);
+
+    relation ComponentType(String, String);
+    relation LoadArrayIndex_Base(String, String);
+    relation LoadArrayIndex_To(String, String);
+    relation StoreArrayIndex_From(String, String);
+    relation StoreArrayIndex_Base(String, String);
+    relation AssignInstruction_To(String, String);
+    relation AssignCast_From(String, String);
+    relation AssignCast_Type(String, String);
+
+    relation AssignLocal_From(String, String);
+    relation AssignHeapAllocation_Heap(String, String);
+    relation ReturnNonvoid_Var(String, String);
+    relation MethodInvocation_Method(String, String);
+    relation AssignReturnValue(String, String);
+    relation ActualParam(String, String, String);
+    relation VirtualMethodInvocation_Base(String, String);
+    relation VirtualMethodInvocation_SimpleName(String, String);
+    relation VirtualMethodInvocation_Descriptor(String, String);
+    relation SpecialMethodInvocation_Base(String, String);
+    relation MethodInvocation_Base(String, String);
+    // Fat schema
+    relation LoadInstanceField(String, String, String, String);
+    relation StoreInstanceField(String, String, String, String);
+    relation LoadStaticField(String, String, String);
+    relation StoreStaticField(String, String, String);
+    relation LoadArrayIndex(String, String, String);
+    relation StoreArrayIndex(String, String, String);
+    relation AssignCast(String, String, String, String);
+    relation AssignLocal(String, String, String);
+    relation AssignHeapAllocation(String, String, String);
+    relation ReturnVar(String, String);
+    relation StaticMethodInvocation(String, String, String);
+
+    relation _ClassType(String);
+    isType(class) <-- _ClassType(class);
+    isReferenceType(class) <-- _ClassType(class);
+    isClassType(class) <-- _ClassType(class);
+    relation _ArrayType(String);
+    isType(arrayType) <-- _ArrayType(arrayType);
+    isReferenceType(arrayType) <-- _ArrayType(arrayType);
+    isArrayType(arrayType) <-- _ArrayType(arrayType);
 }
 
 
 fn input_process(prog: &mut AscentProgram, zip_file_name: &String) {
     //.input DirectSuperclass(IO="file", filename="DirectSuperclass.facts", delimiter="\t")
-    let tmp = utils::utils::read_file_from_zip_to_vec_doop_thin(zip_file_name,"database/DirectSuperclass.facts")
+    let tmp = utils::utils::
+    read_file_from_zip_to_vec_doop_thin(zip_file_name, "database/DirectSuperclass.facts")
         .into_iter().map(|mut v| {
         (v.remove(0), v.remove(0))  // 直接移除并返回向量的前两个元素
     });
     prog.DirectSuperclass.extend(tmp);
-    // println!("[debug]: {:?}",prog.DirectSuperclass.get(0));
     //.input DirectSuperinterface(IO="file", filename="DirectSuperinterface.facts", delimiter="\t")
-    let tmp = utils::utils::read_file_from_zip_to_vec_doop_thin(zip_file_name,"database/DirectSuperinterface.facts")
+    let tmp = utils::utils::
+    read_file_from_zip_to_vec_doop_thin(zip_file_name, "database/DirectSuperinterface.facts")
         .into_iter().map(|mut v| {
         (v.remove(0), v.remove(0))  // 直接移除并返回向量的前两个元素
     });
     prog.DirectSuperinterface.extend(tmp);
+    // .input MainClass(IO="file", filename="MainClass.facts", delimiter="\t")
+    let tmp = utils::utils::
+    read_file_from_zip_to_vec_doop_thin(zip_file_name, "database/MainClass.facts")
+        .into_iter().map(|mut v| {
+        (v.remove(0), )  // 直接移除并返回向量的前两个元素
+    });
+    prog.MainClass.extend(tmp);
+    // .input Method_Modifier(IO="file", filename="Method-Modifier.facts", delimiter="\t")
+    let tmp = utils::utils::
+    read_file_from_zip_to_vec_doop_thin(zip_file_name, "database/Method-Modifier.facts")
+        .into_iter().map(|mut v| {
+        (v.remove(0), v.remove(0))  // 直接移除并返回向量的前两个元素
+    });
+    prog.Method_Modifier.extend(tmp);
+    // .input FormalParam(IO="file", filename="FormalParam.facts", delimiter="\t")
+    let tmp = utils::utils::
+    read_file_from_zip_to_vec_doop_thin(zip_file_name, "database/FormalParam.facts")
+        .into_iter().map(|mut v| {
+        (v.remove(0), v.remove(0), v.remove(0))  // 直接移除并返回向量的前两个元素
+    });
+    prog.FormalParam.extend(tmp);
+    // .input Var_Type(IO="file", filename="Var-Type.facts", delimiter="\t")
+    let tmp = utils::utils::
+    read_file_from_zip_to_vec_doop_thin(zip_file_name, "database/Var-Type.facts")
+        .into_iter().map(|mut v| {
+        (v.remove(0), v.remove(0))  // 直接移除并返回向量的前两个元素
+    });
+    prog.Var_Type.extend(tmp);
+    // .input ComponentType(IO="file", filename="ComponentType.facts", delimiter="\t")
+    let tmp = utils::utils::
+    read_file_from_zip_to_vec_doop_thin(zip_file_name, "database/ComponentType.facts")
+        .into_iter().map(|mut v| {
+        (v.remove(0), v.remove(0))  // 直接移除并返回向量的前两个元素
+    });
+    prog.ComponentType.extend(tmp);
+    // .input AssignReturnValue(IO="file", filename="AssignReturnValue.facts", delimiter="\t")
+    let tmp = utils::utils::
+    read_file_from_zip_to_vec_doop_thin(zip_file_name, "database/AssignReturnValue.facts")
+        .into_iter().map(|mut v| {
+        (v.remove(0), v.remove(0))  // 直接移除并返回向量的前两个元素
+    });
+    prog.AssignReturnValue.extend(tmp);
+    // .input ActualParam(IO="file", filename="ActualParam.facts", delimiter="\t")
+    let tmp = utils::utils::
+    read_file_from_zip_to_vec_doop_thin(zip_file_name, "database/ActualParam.facts")
+        .into_iter().map(|mut v| {
+        (v.remove(0), v.remove(0), v.remove(0))  // 直接移除并返回向量的前两个元素
+    });
+    prog.ActualParam.extend(tmp);
+    // .input _ClassType(IO="file", filename="ClassType.facts", delimiter="\t")
+    let tmp = utils::utils::
+    read_file_from_zip_to_vec_doop_thin(zip_file_name, "database/ClassType.facts")
+        .into_iter().map(|mut v| {
+        (v.remove(0), )  // 直接移除并返回向量的前两个元素
+    });
+    prog._ClassType.extend(tmp);
+    // .input _ArrayType(IO="file", filename="ArrayType.facts", delimiter="\t")
+    let tmp = utils::utils::
+    read_file_from_zip_to_vec_doop_thin(zip_file_name, "database/ArrayType.facts")
+        .into_iter().map(|mut v| {
+        (v.remove(0), )  // 直接移除并返回向量的前两个元素
+    });
+    prog._ArrayType.extend(tmp);
 }
 
 fn main() {
@@ -69,7 +193,7 @@ fn main() {
     prog.run();
     let duration = start.elapsed();
     println!("方法运行时间: {:?}", duration);
-    // println!("{}",prog.DirectSuperinterface.len())
+    // println!("{}",prog.isArrayType.len())
     // for (i, j) in prog.p {
     //     println!("{}, {}", i, j);
     // }
